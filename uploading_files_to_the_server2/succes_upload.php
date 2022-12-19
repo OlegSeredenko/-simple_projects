a2bf0d36c676e039feb453822d6480f20733f9c1
Oleg <80279502+OlegSeredenko@users.noreply.github.com>
2022-12-19 14:43:27 +0300

    Add files via upload

---
+++ uploading_files_to_the_server2/succes_upload.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8" >
+    <title>Ошибка при загрузке</title>
+    <link  rel="stylesheet" href="main.css">
+</head>
+    <body>
+        <?php
+        echo ' <p class="msg_succes"> '. "Файл успешно загружен" . ' </p>' ;
+        
+        ?>
+    </body>
+</html>
