6a5099702429de27f12d07e73ed280557c5d0e8d
Oleg <80279502+OlegSeredenko@users.noreply.github.com>
2022-12-19 14:43:13 +0300

    Add files via upload

---
+++ uploading_files_to_the_server2/error_upload.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8" >
+    <title>Ошибка при загрузке</title>
+    <link  rel="stylesheet" href="main.css">
+</head>
+    <body>
+        <?php
+        echo ' <p class="msg_error"> '. "Файл не был загружен" . ' </p>' ;
+        ?>
+    </body>
+</html>
